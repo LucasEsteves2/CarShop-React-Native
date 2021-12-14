cff084b9ab90cd3abca5398516d6d10ba19dc7e8
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-14 20:53:51 -0300

    funcao de deslogar

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Text } from "react-native";
+import { Alert, Button, Text } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
 export function Home() {
+  const navigation = useNavigation();
   const [usuario, setUsuario] = useState({});
 
   //Pegando dados do usuario logado (async storage)
@@ -10,10 +12,21 @@ export function Home() {
     async function loadData() {
       const usuario2 = await AsyncStorage.getItem("@serratec:usuario");
       setUsuario(JSON.parse(usuario2));
-
     }
     loadData();
   }, []);
 
-  return <Text>{usuario.email}</Text>;
+  function logout()
+{
+  navigation.navigate("Login");
+
 }
+  return (
+    <>
+      <Text>Bem vindo{usuario.email}</Text>
+
+      <Button title="Deslogar" onPress={logout}> </Button>
+    </>
+  );
+}
+
