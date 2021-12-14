04de7f6a83b81da02eb832b91b94e34cae33bea1
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-14 11:27:39 -0300

    add

--- routes/index.jsx
+++ routes/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { createStackNavigator} from '@react-navigation/native-stack';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from  '@react-navigation/stack';
 
+import SignUp from '../src/screen/Login/SignUp';
+import Login from '../src/screen/Login/Login';
 
 const Stack = createStackNavigator();
 
-export const Routes=()=> {
-    return(
-        <Stack.Navigator>
-            <Stack.Screen name=" "  component={  }/>
-            <Stack.Screen name=" "  component={  }/>
-        </Stack.Navigator>
-    )
-}
+const Navigation = props => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Login} options={{headerShown: false}} />
+                <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
