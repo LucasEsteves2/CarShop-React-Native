c6874294cd3ffef60c6638b6a50f0f8b6f2f3b5a
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-14 20:33:39 -0300

    add async Storage

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -3,13 +3,14 @@ import { Alert, Text } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function Home() {
-  const [usuario,setUsuario] =useState()
+  const [usuario, setUsuario] = useState({});
 
   //Pegando dados do usuario logado (async storage)
   useEffect(() => {
     async function loadData() {
-     const usuario= await AsyncStorage.getItem("@serratec:usuario");
-     setUsuario(JSON.parse(usuario))
+      const usuario2 = await AsyncStorage.getItem("@serratec:usuario");
+      setUsuario(JSON.parse(usuario2));
+
     }
     loadData();
   }, []);
