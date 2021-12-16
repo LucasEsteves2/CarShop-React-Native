7d675ac5df53cbc2dadf66b184fc5250df4b2049
letilannes <leticiasogomes@gmail.com>
2021-12-15 21:55:17 -0300

    upDate

---
+++ src/screens/upDate/index.js
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+import { api } from '../../service/apiSerratec';
+
+export function upDate() {
+    const [id, setId] = useState('');
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao]= useState('');
+    const[imagem, setImagem]= useState('');
+    const[valor, setValor]= useState('');
+    const[qtdEstoque, setQtdEstoque]= useState('');
+  
+    async function Atualizar(){
+        await api.put(`/produto/${id}`,{
+            descricao: descricao,
+            fotoLink: imagem,
+            idCategoria: 1,
+            idFuncionario: 1,
+            nome: nome,
+            nomeCategoria: "aaaaaaaaaaaaaaaaaaaaaaaaaa",
+            nomeFuncionario: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            qtdEstoque: qtdEstoque,
+            valor: valor
+        })
+    }
+  
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden/>
+        <TextInput placeholder='id' style={styles.textImput} onChangeText={text=> setId(text)}/>
+        <TextInput placeholder='nome' style={styles.textImput} onChangeText={text=> setNome(text)}/>
+        <TextInput placeholder='descricao' style={styles.textImput} onChangeText={text=> setDescricao(text)}/>
+        <TextInput placeholder='imagem' style={styles.textImput} onChangeText={text=> setImagem(text)}/>
+        <TextInput placeholder='valor' style={styles.textImput} onChangeText={text=> setValor(text)}/>
+        <TextInput placeholder='qtdEstoque' style={styles.textImput} onChangeText={text=> setQtdEstoque(text)}/>
+  
+        <TouchableOpacity onPress={Atualizar} style={styles.bntUpdate} >
+          <Text style={{color:'white',textAlign:'center'}}>Update do produto</Text>
+        </TouchableOpacity>
+  
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#613FA0",
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textImput:{
+    paddingVertical: 10,
+      width:'100%',
+      height:40,
+      backgroundColor:'white',
+      borderRadius:20,
+      paddingLeft:10,
+      paddingBottom:10
+    },
+    bntUpdate:{
+      width:'100%',
+      height:40,
+      backgroundColor:'#7b42f5',
+      borderRadius:20,
+      justifyContent:'center'
+    }
+  });
+  
