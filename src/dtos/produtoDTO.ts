d8f988471f29b0f38d72a81133421e1d770fc4ed
natalino <natalinoesteves2010@gmail.com>
2021-12-16 17:04:18 -0300

    add flat

---
+++ src/dtos/produtoDTO.ts
@@ -0,0 +1,10 @@
+export interface ProdutoDTO {
+    id: bigint;
+    nome: string;
+    valor: number;
+    urlImagem: "http://2.bp.blogspot.com/-ldGXEgsKb-8/UGWaoz18JBI/AAAAAAAAAKo/hji598eRG2k/s1600/png_carro.png",
+    desc: string[];
+
+
+}
+
