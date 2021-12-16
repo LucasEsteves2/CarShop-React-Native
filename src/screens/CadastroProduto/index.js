bcd315122c6c5ed38ef07dd1d572fd19349c52cf
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-16 20:16:38 -0300

    bug

--- src/screens/CadastroProduto/index.js
+++ src/screens/CadastroProduto/index.js
@@ -1,160 +1,137 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  View,
-  TouchableOpacity,
-  TextInput,
-} from "react-native";
+import React, {useState} from 'react';
+import { StyleSheet, Text, SafeAreaView, View, TouchableOpacity, TextInput} from 'react-native';
 import { api } from "../../service/api";
+import logozita from "../../imagens/img2.png";
 
 export function Cadastrar() {
-  const [Descricao, setDescricao] = useState("");
-  const [Foto, setfoto] = useState("");
-  const [Nome, setNome] = useState("");
-  const [Qtdestoque, setQtdestoque] = useState("");
-  const [Valor, setValor] = useState("");
 
-  async function pegarproduto() {
-    try {
-      alert("Sucesso! Produto cadastrado!");
-      const { data } = await api.post("/produtos", {
-        desc: "66666666666",
-        nome: "55555555555 car",
-        qtd_estoque: 15,
-        urlImagem:
-          "https://www.imagensempng.com.br/wp-content/uploads/2021/08/Carro-policia-Png-1024x1024.png",
-        valor: 499.126,
-      });
-    } catch (error) {
-      alert(error.toString());
-    }
-  }
+  const [descricao,setDescricao]=useState("");
+  const [foto,setfoto]=useState("");;
+  const [nome,setNome]=useState("");
+  const [qtdestoque,setQtdestoque]=useState("");
+  const [valor,setValor]=useState("");
 
+   async function pegarproduto(){
+     try{
+       alert("Sucesso! Produto cadastrado!")
+   const {data}= await api.post("/produtos", 
+   {   
+   desc: descricao,
+   nome: nome,
+   qtd_estoque: qtdestoque,
+   urlImagem: foto,
+   valor: valor
+  })
+
+      }catch(error){
+        alert(error.toString())
+      }
+
+  };
+ 
+  
   return (
-    <SafeAreaView style={styles.area}>
-      <View style={styles.containerProd}>
-        <Text style={styles.textcadastro}>CADASTRO DE NOVOS PRODUTOS</Text>
+    <View style={styles.container}>
+      <View style={styles.imageview}>
+        <Image source={logozita} style={styles.imagem} />
       </View>
-      <View style={styles.containerProd}>
-        <Text style={styles.legenda}>Nome do produto: </Text>
+
+      <View>
+        <Text style={styles.title}>Nome:</Text>
         <TextInput
-          style={styles.input}
-          placeholder="Nome"
-          placeholderTextColor="#C3C3C3"
           onChangeText={setNome}
-        />
-      </View>
-
-      <View style={styles.containerProd}>
-        <Text style={styles.legenda}>Link da imagem do produto: </Text>
-        <TextInput
           style={styles.input}
-          placeholder="Imagem"
+          placeholder="Usuario"
           placeholderTextColor="#C3C3C3"
-          onChangeText={setfoto}
         />
       </View>
 
-      <View style={styles.containerProd}>
-        <Text style={styles.legenda}>Descrição do produto: </Text>
+      <View style={styles.senha}>
+        <Text style={styles.title}>E-mail:</Text>
         <TextInput
-          style={styles.inputDesc}
-          multiline
-          placeholder="Descrição"
-          placeholderTextColor="#C3C3C3"
-          onChangeText={setDescricao}
-        />
-      </View>
-
-      <View style={styles.containerProd}>
-        <Text style={styles.legenda}>Quantidade do produto: </Text>
-        <TextInput
+          onChangeText={setEmail}
           style={styles.input}
-          placeholder="Quantidade"
+          placeholder="E-mail"
           placeholderTextColor="#C3C3C3"
-          onChangeText={setQtdestoque}
         />
       </View>
 
-      <View style={styles.containerProd}>
-        <Text style={styles.legenda}>Valor do produto: </Text>
+      <View style={styles.senha}>
+        <Text style={styles.title}>Senha:</Text>
         <TextInput
+          onChangeText={setSenha}
           style={styles.input}
-          placeholder="Valor"
+          placeholder="Senha"
+          secureTextEntry={true}
           placeholderTextColor="#C3C3C3"
-          onChangeText={setValor}
         />
       </View>
 
-      <View style={styles.containerProd}>
-        <TouchableOpacity style={styles.botao} onPress={pegarproduto}>
-          <Text style={styles.add}>ADICIONAR PRODUTO</Text>
+      <View>
+        <TouchableOpacity style={styles.butao} onPress={cadastrar}>
+          <Text style={styles.btntext}>Cadastrar</Text>
         </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
+
+
+function modalAlert(msg) {
+  Alert.alert("#Error404", msg, [
+    { text: "OK", onPress: () => console.log("OK Pressed") },
+  ]);
+}
 const styles = StyleSheet.create({
-  area: {
-    width: "100%",
-    height: "100%",
+  container: {
+    flex: 1,
     backgroundColor: "#613FA0",
+    paddingHorizontal: 30,
+    paddingVertical: 70,
   },
+  senha: {
+    paddingVertical: 9,
+  },
+  title: {
+    color: "#FFFFFF",
+    fontSize: 9,
+    fontWeight: "bold",
+    paddingVertical: 9,
+  },
+  btntext: {
+    fontSize: 11,
+    color: "#FFFFFF",
+  },
+
   input: {
-    width: "80%",
-    height: 30,
-    backgroundColor: "#ffffff",
-    borderColor: "#ffffff",
-    borderWidth: 3,
-    fontSize: 15,
-    borderRadius: 20,
+    backgroundColor: "#FFFFFF",
+    color: "black",
     padding: 9,
+    borderRadius: 7,
+    marginTop: 3,
+    fontSize: 10,
   },
-  inputDesc: {
-    width: "80%",
-    height: 50,
-    backgroundColor: "#ffffff",
-    borderColor: "#fff",
-    borderWidth: 3,
-    fontSize: 15,
-    borderRadius: 20,
-    padding: 9,
-  },
-  legenda: {
-    width: "80%",
-    marginLeft: 20,
-    fontSize: 17,
-    color: "#fff",
-    fontWeight: "bold",
-    marginTop: 15,
-  },
-  botao: {
-    flexDirection: "row",
+  butao: {
+    backgroundColor: "#A370F7",
+    padding: 10,
+    borderRadius: 7,
+    alignItems: "center",
     marginTop: 20,
-    width: "80%",
-    height: 30,
-    backgroundColor: "#7b42f5",
-    borderRadius: 20,
+  },
+  imagem: {
     justifyContent: "center",
-    justifyContent: "center",
+    marginVertical: 20,
+
+    width: 180,
+    height: 150,
+  },
+  imageview: {
     alignItems: "center",
   },
-  add: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  containerProd: {
-    justifyContent: "center",
+
+  cadastro: {
+    paddingTop: 25,
     alignItems: "center",
-    marginTop: 15,
-  },
-  textcadastro: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 30,
-    color: "#fff",
   },
 });
+
