082465935a2c2391c66e10c36457f752ac94b03f
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-16 00:45:15 -0300

    att api

--- routes/routes.tsx
+++ routes/routes.tsx
@@ -10,13 +10,11 @@ const Stack = createStackNavigator();
 
 export const Routes=()=>{
     return(
-
     <Stack.Navigator>
        <Stack.Screen name="Login" component={Login} options={{headerShown:false,}}/>
        <Stack.Screen name="Home" component={BottomTabNavigator} options={{headerShown:false,}}/>
          <Stack.Screen name="Cadastrar" component={Cadastro} options={{headerShown:false,}}/>
          <Stack.Screen name="Editar" component={upDate} options={{headerShown:false,}}/>
-
     </Stack.Navigator>
 
     );
--- src/screens/upDate/index.js
+++ src/screens/upDate/index.js
@@ -1,70 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
-import { api } from '../../service/apiSerratec';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { api } from "../../service/api";
 
 export function upDate() {
-    const [id, setId] = useState('');
-    const [nome, setNome] = useState('');
-    const [descricao, setDescricao]= useState('');
-    const[imagem, setImagem]= useState('');
-    const[valor, setValor]= useState('');
-    const[qtdEstoque, setQtdEstoque]= useState('');
-  
-    async function Atualizar(){
-        await api.put(`/produto/${id}`,{
-            descricao: descricao,
-            fotoLink: imagem,
-            idCategoria: 1,
-            idFuncionario: 1,
-            nome: nome,
-            nomeCategoria: "aaaaaaaaaaaaaaaaaaaaaaaaaa",
-            nomeFuncionario: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            qtdEstoque: qtdEstoque,
-            valor: valor
-        })
-    }
-  
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden/>
-        <TextInput placeholder='id' style={styles.textImput} onChangeText={text=> setId(text)}/>
-        <TextInput placeholder='nome' style={styles.textImput} onChangeText={text=> setNome(text)}/>
-        <TextInput placeholder='descricao' style={styles.textImput} onChangeText={text=> setDescricao(text)}/>
-        <TextInput placeholder='imagem' style={styles.textImput} onChangeText={text=> setImagem(text)}/>
-        <TextInput placeholder='valor' style={styles.textImput} onChangeText={text=> setValor(text)}/>
-        <TextInput placeholder='qtdEstoque' style={styles.textImput} onChangeText={text=> setQtdEstoque(text)}/>
-  
-        <TouchableOpacity onPress={Atualizar} style={styles.bntUpdate} >
-          <Text style={{color:'white',textAlign:'center'}}>Update do produto</Text>
-        </TouchableOpacity>
-  
-      </View>
-    );
+  const [id, setId] = useState("");
+  const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [valor, setValor] = useState("");
+  const [qtdEstoque, setQtdEstoque] = useState("");
+
+  var data = {
+    descricao: descricao,
+    nome: nome,
+    qtd_estoque: qtdEstoque,
+    urlImg: imagem,
+    valor: valor,
+  };
+
+  async function Atualizar() {
+    await api.put(`/produtos/${id}`, data);
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#613FA0",
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    textImput:{
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <TextInput
+        placeholder="id"
+        style={styles.textImput}
+        onChangeText={(text) => setId(text)}
+      />
+      <TextInput
+        placeholder="nome"
+        style={styles.textImput}
+        onChangeText={(text) => setNome(text)}
+      />
+      <TextInput
+        placeholder="descricao"
+        style={styles.textImput}
+        onChangeText={(text) => setDescricao(text)}
+      />
+      <TextInput
+        placeholder="imagem"
+        style={styles.textImput}
+        onChangeText={(text) => setImagem(text)}
+      />
+      <TextInput
+        placeholder="valor"
+        style={styles.textImput}
+        onChangeText={(text) => setValor(text)}
+      />
+      <TextInput
+        placeholder="qtdEstoque"
+        style={styles.textImput}
+        onChangeText={(text) => setQtdEstoque(text)}
+      />
+
+      <TouchableOpacity onPress={Atualizar} style={styles.bntUpdate}>
+        <Text style={{ color: "white", textAlign: "center" }}>
+          Update do produto
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#613FA0",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textImput: {
     paddingVertical: 10,
-      width:'100%',
-      height:40,
-      backgroundColor:'white',
-      borderRadius:20,
-      paddingLeft:10,
-      paddingBottom:10
-    },
-    bntUpdate:{
-      width:'100%',
-      height:40,
-      backgroundColor:'#7b42f5',
-      borderRadius:20,
-      justifyContent:'center'
-    }
-  });
-  
+    width: "100%",
+    height: 40,
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingLeft: 10,
+    paddingBottom: 10,
+  },
+  bntUpdate: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#7b42f5",
+    borderRadius: 20,
+    justifyContent: "center",
+  },
+});
